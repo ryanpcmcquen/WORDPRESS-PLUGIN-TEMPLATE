d347d9a87cb8c1d0cb53fa5595afdf64da2404a1
Ryan P.C. McQuen <ryan.q@linux.com>
2016-01-26 07:32:49 -0800

    Create WORDPRESS-PLUGIN-TEMPLATE.php

---
+++ WORDPRESS-PLUGIN-TEMPLATE.php
@@ -0,0 +1,14 @@
+<?php
+/*
+** Plugin Name: WORDPRESS PLUGIN TEMPLATE
+** Plugin URI: https://github.com/ryanpcmcquen/WORDPRESS-PLUGIN-TEMPLATE
+** Description: AWESOME DESCRIPTION HERE.
+** Version: 1.0.0
+** Author: Ryan P.C. McQuen
+** Author URI: ryanpcmcquen.org
+** License: GPLv2 or later
+** License URI: http://www.gnu.org/licenses/gpl-2.0.html
+** TextDomain: WORDPRESS-PLUGIN-TEMPLATE
+** DomainPath:
+** Network:
+*/
