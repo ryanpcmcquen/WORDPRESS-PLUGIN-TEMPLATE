70d8f7f546c105131a8792abad532a9f5aead01f
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-08-02 05:55:21 -0700

    P. C.

--- WORDPRESS-PLUGIN-TEMPLATE.php
+++ WORDPRESS-PLUGIN-TEMPLATE.php
@@ -4,7 +4,7 @@
 ** Plugin URI: https://github.com/ryanpcmcquen/WORDPRESS-PLUGIN-TEMPLATE
 ** Description: AWESOME DESCRIPTION HERE.
 ** Version: 1.0.0
-** Author: Ryan P.C. McQuen
+** Author: Ryan P. C. McQuen
 ** Author URI: ryanpcmcquen.org
 ** License: GPLv2 or later
 ** License URI: http://www.gnu.org/licenses/gpl-2.0.html
